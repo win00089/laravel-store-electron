9138f86fa8ac7c347cf1cd3b98aad34e45dfcb45
Nik Bel <bf109e1111@gmail.com>
2025-03-17 23:12:41 +0300

    feat: add custom class Basket and Model injection

--- app/Classes/Basket.php
+++ app/Classes/Basket.php
@@ -3,17 +3,65 @@
 namespace App\Classes;
 
 use App\Models\Order;
+use App\Models\Product;
+use Illuminate\Support\Facades\Auth;
 
-class Basket{
+class Basket
+{
   protected $order;
 
-  public function __construct()
+  public function __construct($createOrder = false)
   {
     $orderId = session('orderId');
-    $this->order = Order::findOrFail($orderId);
+    if (is_null($orderId) && $createOrder) {
+      $data = [];
+      if (Auth::check()) {
+        $data['user_id'] = Auth::id();
+      }
+      $this->order = Order::create($data);
+      session(['orderId' => $this->order->id]);
+    } else {
+      $this->order = Order::findOrFail($orderId);
+    }
   }
 
-  public function getOrder(){
+  public function getOrder()
+  {
     return $this->order;
+  }
+
+  public function saveOrder($name, $phone)
+  {
+    return $this->order->saveOrder($name, $phone);
+  }
+
+  protected function getPivotRow($product){
+    return $this->order->products()->where('product_id', $product->id)->first()->pivot;
+  }
+
+  public function removeProduct(Product $product)
+  {
+    if ($this->order->products->contains($product->id)) {
+      $pivotRow = $this->getPivotRow($product);
+      if ($pivotRow->count < 2) {
+        $this->order->products()->detach($product->id);
+      } else {
+        $pivotRow->count--;
+        $pivotRow->update();
+      }
+    }
+  }
+
+  public function addProduct(Product $product)
+  {
+    if ($this->order->products->contains($product->id)) {
+      $pivotRow = $this->getPivotRow($product);
+      $pivotRow->count++;
+      $pivotRow->update();
+    } else {
+      $this->order->products()->attach($product->id);
+    }
+
+    Order::changeFullSum($product->price);
   }
 }
--- app/Http/Controllers/BasketController.php
+++ app/Http/Controllers/BasketController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Classes\Basket;
 use App\Models\Order;
 use App\Models\Product;
 use Illuminate\Http\Request;
@@ -11,23 +12,16 @@ class BasketController extends Controller
 {
     public function basket()
     {
-        $orderID = session('orderId');
+        $order = (new Basket())->getOrder();
         
-            $order = Order::findOrFail($orderID);
-        
-       
         return view('basket', compact('order'));
     }
 
     public function basketConfirm(Request $request)
     {
-        $orderID = session('orderId');
-        $order = Order::findOrFail($orderID);
-        $success = $order->saveOrder($request->name, $request->phone);
-
-        if($success){
+        if ((new Basket())->saveOrder($request->name, $request->phone)) {
             session()->flash('success', 'Ваш заказ принят в обработку');
-        }else{
+        } else {
             session()->flash('warning', 'Случилась ошибка');
         }
         Order::eraseOrderSum();
@@ -35,56 +29,21 @@ class BasketController extends Controller
     }
     public function basketPlace()
     {
-        $orderID = session('orderId');
-        $order = Order::findOrFail($orderID);
+        $order = (new Basket())->getOrder();
         return view('order', compact('order'));
     }
     public function basketAdd(Product $product)
     {
-
+        (new Basket(true))->addProduct($product);
         
-        $orderID = session('orderId');
-        if (is_null($orderID)) {
-            $order = Order::create();
-            session(['orderId' => $order->id]);
-        } else {
-            $order = Order::find($orderID);
-        }
-
-        if ($order->products->contains($product->id)) {
-            $pivotRow = $order->products()->where('product_id', $product->id)->first()->pivot;
-            $pivotRow->count++;
-            $pivotRow->update();
-        } else {
-            $order->products()->attach($product->id);
-        }
-
-        if(Auth::check()){
-            $order->user_id = Auth::id();
-            $order->save();
-        }
-
-        Order::changeFullSum($product->price);
         session()->flash('success', 'Добавлен товар ' . $product->name);
-
         return redirect()->route('basket');
     }
     public function basketRemove(Product $product)
     {
-        $orderID = session('orderId');
-        $order = Order::findOrFail($orderID);
-        if ($order->products->contains($product->id)) {
-            $pivotRow = $order->products()->where('product_id', $product->id)->first()->pivot;
-            if ($pivotRow->count < 2) {
-                $order->products()->detach($product->id);
-            } else {
-                $pivotRow->count--;
-                $pivotRow->update();
-            }
-        }
-        Order::changeFullSum(-$product->price);
+        (new Basket())->removeProduct($product);
+        
         session()->flash('warning', 'Удален товар ' . $product->name);
-
         return redirect()->route('basket');
     }
 }
