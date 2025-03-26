476cc6d7cf009af82c970385b04f2ae5b390cf95
Nik Bel <bf109e1111@gmail.com>
2025-03-26 23:16:29 +0300

    refactor: Added the product properties participating in the product offer in the admin panel

---
+++ app/Http/Controllers/Admin/SkuController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Sku;
+use Illuminate\Http\Request;
+
+class SkuController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Sku  $sku
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Sku $sku)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Sku  $sku
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Sku $sku)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Sku  $sku
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Sku $sku)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Sku  $sku
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Sku $sku)
+    {
+        //
+    }
+}
