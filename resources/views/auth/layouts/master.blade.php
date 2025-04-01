155d8df95316fdf156997d3cccb00f809e4e9ea6
Nik Bel <bf109e1111@gmail.com>
2025-04-01 16:40:04 +0300

    Creating a token for authorization. Configuring guard & provider for authorization under the Merchant model

--- resources/views/auth/layouts/master.blade.php
+++ resources/views/auth/layouts/master.blade.php
@@ -43,6 +43,7 @@
                         <li><a href="{{ route('products.index') }}">Товары</a></li>
                         <li><a href="{{ route('properties.index') }}">Свойства</a></li>
                         <li><a href="{{ route('coupons.index') }}">Купоны</a></li>
+                        <li><a href="{{ route('merchants.index') }}">Поставщики</a>
                         <li><a href="{{ route('home') }}">Заказы</a></li>
                         @endadmin
                     </ul>
